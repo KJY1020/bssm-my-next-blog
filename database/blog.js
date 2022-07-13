3c793e50543540f3a79782d874527f2efb1ac0cb
pkiop <pkiopb@gmail.com>
2022-07-13 14:35:11 +0900

    test

--- database/blog.js
+++ database/blog.js
@@ -2,14 +2,14 @@ export const blogList = [
   {
     id: 1,
     title: 'Nextjs 쓰는 법',
-    content: `Nextjs는 짱짱 좋습니다.`,
+    content: `Nextjs`,
     readTime: 1,
     writer: 'pkiop',
   },
   {
     id: 2,
     title: 'Nextjs 장점',
-    content: `귀찮은 거 다 해줌`,
+    content: `귀찮은`,
     readTime: 2,
     writer: 'pkiop',
   },
