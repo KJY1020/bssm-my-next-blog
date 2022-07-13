f68075a79151faa240e9a43df22ce3957291adc2
pkiop <pkiopb@gmail.com>
2022-07-13 12:03:47 +0900

    Feat:

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,9 +1,22 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
   const router = useRouter();
   const { id } = router.query;
-  return <div>블로그 {id}입니다. </div>;
+  const [blog, setBlog] = useState();
+
+  const fetchData = async () => {
+    const result = await axios.get(`/api/blog/${id}`);
+    setBlog(result.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <div>{JSON.stringify(blog)}</div>;
 }
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,9 +3,11 @@ import Image from 'next/image';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import styles from './index.module.css';
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const [blogList, setBlogList] = useState([]);
+  const router = useRouter();
 
   const fetchData = async () => {
     const result = await axios.get('/api/blog');
@@ -16,12 +18,20 @@ export default function Home() {
     fetchData();
   }, []);
 
+  const clickBlog = (id) => () => {
+    router.push(`/blog/${id}`);
+  };
+
   return (
     <div>
       블로그 목록입니다.
       {blogList.map((blog) => {
         return (
-          <div className={styles.blog} key={blog.id}>
+          <div
+            className={styles.blog}
+            key={blog.id}
+            onClick={clickBlog(blog.id)}
+          >
             <div>{blog.title}</div>
           </div>
         );
